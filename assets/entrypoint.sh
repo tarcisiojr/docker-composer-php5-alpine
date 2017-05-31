41c315f838d3b3b9af015c9abb07d3f41d7ab54e
Tarcísio Júnior <tarcisio@softbox.com.br>
2017-05-31 14:33:56 -0300

    Initial commit.

---
+++ assets/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+USER_ID=${LOCAL_USER_ID:-9001}
+
+echo "=> Starting with UID : $USER_ID"
+
+#useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
+adduser -D -u $USER_ID user
+
+export HOME=/home/user
+
+mkdir -p /home/user
+
+chown $USER_ID:$USER_ID $COMPOSER_CACHE_DIR
+
+gosu user composer "$@"
